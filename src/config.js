9de2cbd9d4f93899338233d184c329236f92a9d5
Herson Salinas <hersonhn@gmail.com>
2019-01-16 02:16:31 -0600

    configurable api url

--- src/config.js
+++ src/config.js
@@ -1,4 +1,8 @@
 
-const api = (process.env.NODE_ENV == 'production') ? '' : 'http://localhost:4000';
+var api = (process.env.NODE_ENV == 'production') ? '' : 'http://localhost:4000';
+
+if (process.env.SHORTENER_API_URL) {
+  api = process.env.SHORTENER_API_URL;
+}
 
 export const API = api;
